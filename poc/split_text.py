1bddb8ee1477662f9975b76d4115c773307b60b5
barna22 <szubar10@gmail.com>
2022-03-25 18:31:03 +0100

    create splitter script

---
+++ poc/split_text.py
@@ -0,0 +1,20 @@
+import re
+
+f = open('logs/WCG100140020.txt', 'r')
+log_text = f.read()
+
+f.close()
+
+all_messages = re.split("\d{4}\/[a-zA-Z]{3}\/\d{2} \d{2}:\d{2}:\d{2}.\d{6}", log_text)
+
+print(all_messages[0])
+
+sip_messages = []
+
+for item in all_messages:
+    if ("PORTEVENT" in item or "TIMEROP" in item) and "internalMessage" in item:
+        sip_messages.append(item)
+
+print(len(sip_messages))
+print(sip_messages[0])
+
